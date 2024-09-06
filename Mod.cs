ec754be7908d7643e1cb0b5670521eb2fa0e7cb0
quackandcheese <quackandcheeseart@gmail.com>
2024-09-06 09:15:54 -0500

    make methods for ease of addition

--- Mod.cs
+++ Mod.cs
@@ -50,177 +50,153 @@ namespace KitchenPizzaPlus
             Events.BuildGameDataEvent += delegate (object s, BuildGameDataEventArgs args)
             {
                 ItemGroup rawPizza = (ItemGroup)GDOUtils.GetExistingGDO(ItemReferences.PizzaRaw);
-                GameObject rawPizzaGo = rawPizza.Prefab;
 
                 ItemGroup cookedPizza = (ItemGroup)GDOUtils.GetExistingGDO(ItemReferences.PizzaCooked);
-                GameObject cookedPizzaGo = cookedPizza.Prefab;
 
                 ItemGroup pizzaSlice = (ItemGroup)GDOUtils.GetExistingGDO(ItemReferences.PizzaSlice);
-                GameObject pizzaSliceGo = pizzaSlice.Prefab;
 
                 ItemGroup platedPizzaSlice = (ItemGroup)GDOUtils.GetExistingGDO(ItemReferences.PizzaPlated);
-                GameObject platedPizzaSliceGo = platedPizzaSlice.Prefab;
                 // Create new alfredo sauce for each slice
 
                 if (args.firstBuild)
                 {
-                    // Raw
-                    List<GameObject> alfredoSauces = new List<GameObject>();
-                    foreach (ItemGroupView sliceItemGroupView in rawPizzaGo.GetComponentsInChildren<ItemGroupView>())
-                    {
-                        GameObject alfredoGo = GameObject.Instantiate(Bundle.LoadAsset<GameObject>("AlfredoSauce")).AssignMaterialsByNames();
-                        alfredoGo.transform.SetParent(sliceItemGroupView.transform.Find("Slice"), false);
-                        alfredoSauces.Add(alfredoGo);
-                    }
-
-                    // Add roux + alfredo sauce game object to component groups, replicating tomato sauce
-                    rawPizzaGo.GetComponent<ItemGroupView>().ComponentGroups.Add(new()
-                    {
-                        DrawAll = true,
-                        Objects = alfredoSauces,
-                        Item = (Item)GDOUtils.GetExistingGDO(ItemReferences.RouxPortion),
-                    });
-
-
-                    // Cooked
-                    List<GameObject> cookedAlfredoSauces = new List<GameObject>();
-                    foreach (ItemGroupView sliceItemGroupView in cookedPizzaGo.GetComponentsInChildren<ItemGroupView>())
-                    {
-                        GameObject alfredoGo = GameObject.Instantiate(Bundle.LoadAsset<GameObject>("AlfredoSauce")).AssignMaterialsByNames();
-                        alfredoGo.transform.SetParent(sliceItemGroupView.transform.Find("Slice"), false);
-                        cookedAlfredoSauces.Add(alfredoGo);
-                    }
-
-                    // Add roux + alfredo sauce game object to component groups, replicating tomato sauce
-                    cookedPizzaGo.GetComponent<ItemGroupView>().ComponentGroups.Add(new()
-                    {
-                        DrawAll = true,
-                        Objects = cookedAlfredoSauces,
-                        Item = (Item)GDOUtils.GetExistingGDO(ItemReferences.RouxPortion),
-                    });
-                    
-
-                    GameObject alfredoGoSlice = GameObject.Instantiate(Bundle.LoadAsset<GameObject>("AlfredoSauce")).AssignMaterialsByNames();
-                    alfredoGoSlice.transform.SetParent(pizzaSliceGo.transform.Find("Slice"), false);
-                    pizzaSliceGo.GetComponent<ItemGroupView>().ComponentGroups.Add(new()
-                    {
-                        DrawAll = true,
-                        GameObject = alfredoGoSlice,
-                        Item = (Item)GDOUtils.GetExistingGDO(ItemReferences.RouxPortion),
-                    });
-                    pizzaSliceGo.GetComponent<ItemGroupView>().ComponentGroups.Add(new()
-                    {
-                        DrawAll = true,
-                        GameObject = pizzaSliceGo.transform.Find("Slice/Sauce").gameObject,
-                        Item = (Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce),
-                    });
-
-
-                    GameObject alfredoGoPlatedSlice = GameObject.Instantiate(Bundle.LoadAsset<GameObject>("AlfredoSauce")).AssignMaterialsByNames();
-                    alfredoGoPlatedSlice.transform.SetParent(platedPizzaSliceGo.transform.Find("Pizza Slice/Slice"), false);
-                    platedPizzaSliceGo.GetComponent<ItemGroupView>().ComponentGroups.Add(new()
-                    {
-                        DrawAll = true,
-                        GameObject = alfredoGoPlatedSlice,
-                        Item = (Item)GDOUtils.GetExistingGDO(ItemReferences.RouxPortion),
-                    });
-                    platedPizzaSliceGo.GetComponent<ItemGroupView>().ComponentGroups.Add(new()
-                    {
-                        DrawAll = true,
-                        GameObject = platedPizzaSliceGo.transform.Find("Pizza Slice/Slice/Sauce").gameObject,
-                        Item = (Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce),
-                    });
-
-                    // Add labels
-                    AddColorblindLabel(rawPizzaGo);
-                    AddColorblindLabel(cookedPizzaGo);
-                    //AddColorblindLabel(pizzaSliceGo);
-                    Item steak = (Item)GDOUtils.GetExistingGDO(ItemReferences.SteakMedium);
-                    if (steak != null)
-                    {
-                        GameObject ColorBlind = GameObject.Instantiate(steak.Prefab.transform.Find("Colour Blind").gameObject);
-                        ColorBlind.name = "Colour Blind";
-                        ColorBlind.transform.SetParent(pizzaSliceGo.transform);
-                        ColorBlind.transform.Find("Title").GetComponent<TMP_Text>().text = "Alf";
-                    }
-                    AddColorblindLabel(platedPizzaSliceGo);
+                    SetupPizzaPrefab(rawPizza, "AlfredoSauce", ItemReferences.RouxPortion, "Alf");
+                    SetupPizzaPrefab(cookedPizza, "AlfredoSauce", ItemReferences.RouxPortion, "Alf");
+                    SetupPizzaPrefab(pizzaSlice, "AlfredoSauce", ItemReferences.RouxPortion, "Alf", isSlice: true);
+                    SetupPizzaPrefab(platedPizzaSlice, "AlfredoSauce", ItemReferences.RouxPortion, "Alf", "Pizza Slice/Slice", isSlice: true);
                 }
 
-
-                // Remove tomato sauce from ItemSet with crust and sauce to make new ItemSet with tomato sauce OR roux
-
-                ItemSet rawPizzaItemSet = rawPizza.DerivedSets[1];
-                Item tomatoSauce = (Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce);
-                if (rawPizzaItemSet.Items.Contains(tomatoSauce))
-                {
-                    rawPizzaItemSet.Items.Remove(tomatoSauce);
-                    rawPizzaItemSet.Max = 1;
-                    rawPizzaItemSet.Min = 1;
-                    rawPizza.DerivedSets[1] = rawPizzaItemSet;
-                }
-                
-                rawPizza.DerivedSets.Add(new()
-                {
-                    Max = 1,
-                    Min = 1,
-                    RequiresUnlock = false,
-                    IsMandatory = true,
-                    Items = new List<Item>()
-                    {
-                        (Item)GDOUtils.GetExistingGDO(ItemReferences.RouxPortion),
-                        (Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce)
-                    }
-                });
-
-
-                ItemSet platedPizzaItemSet = platedPizzaSlice.DerivedSets[0];
-                if (platedPizzaItemSet.Items.Contains(tomatoSauce))
-                {
-                    platedPizzaItemSet.Items.Remove(tomatoSauce);
-                    platedPizzaItemSet.Max = 1;
-                    platedPizzaItemSet.Min = 1;
-                    platedPizzaSlice.DerivedSets[0] = platedPizzaItemSet;
-                }
-
-                platedPizzaSlice.DerivedSets.Add(new()
-                {
-                    Max = 1,
-                    Min = 1,
-                    RequiresUnlock = false,
-                    IsMandatory = true,
-                    Items = new List<Item>()
-                    {
-                        (Item)GDOUtils.GetExistingGDO(ItemReferences.RouxPortion),
-                        (Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce)
-                    }
-                });
-
-
-                /* ReflectionUtils.GetField<List<ItemGroupView.ColourBlindLabel>>("ComponentLabels").SetValue(rawPizzaGo, new List<ItemGroupView.ColourBlindLabel>()
-                 {
-                     new()
-                     {
-                         Item = (Item)GDOUtils.GetExistingGDO(ItemReferences.RouxPortion),
-                         Text = "Alf"
-                     }
-                 });*/
+                SetupPizzaItemSets(rawPizza, ItemReferences.RouxPortion);
+                SetupPizzaItemSets(platedPizzaSlice, ItemReferences.RouxPortion);
             };
         }
 
-        public void AddColorblindLabel(GameObject go)
+        public void AddColorblindLabel(Item pizzaItem, int itemReference, String label)
         {
-            ItemGroupView itemGroupView = go.GetComponent<ItemGroupView>();
-            FieldInfo componentLabelsField = ReflectionUtils.GetField<ItemGroupView>("ComponentLabels");
-            List<ItemGroupView.ColourBlindLabel> colorblindLabels = componentLabelsField.GetValue(itemGroupView) as List<ItemGroupView.ColourBlindLabel>;
+            GameObject go = pizzaItem.Prefab;
 
-            colorblindLabels.Add(new()
+            if (pizzaItem is ItemGroup)
             {
-                Item = (Item)GDOUtils.GetExistingGDO(ItemReferences.RouxPortion),
-                Text = "Alf"
-            }
-            );
+                ItemGroupView itemGroupView = go.GetComponent<ItemGroupView>();
+                FieldInfo componentLabelsField = ReflectionUtils.GetField<ItemGroupView>("ComponentLabels");
+                List<ItemGroupView.ColourBlindLabel> colorblindLabels = componentLabelsField.GetValue(itemGroupView) as List<ItemGroupView.ColourBlindLabel>;
 
-            componentLabelsField.SetValue(itemGroupView, colorblindLabels);
+                colorblindLabels.Add(new()
+                {
+                    Item = (Item)GDOUtils.GetExistingGDO(itemReference),
+                    Text = label
+                }
+                );
+
+                componentLabelsField.SetValue(itemGroupView, colorblindLabels);
+            }
+            else
+            {
+                /*Item steak = (Item)GDOUtils.GetExistingGDO(ItemReferences.SteakMedium);
+                if (steak != null)
+                {
+                    GameObject ColorBlind = GameObject.Instantiate(steak.Prefab.transform.Find("Colour Blind").gameObject);
+                    ColorBlind.name = "Colour Blind";
+                    ColorBlind.transform.SetParent(go.transform);
+                    ColorBlind.transform.Find("Title").GetComponent<TMP_Text>().text = label;
+                }*/
+            }
+        }
+
+        public void SetupPizzaPrefab(Item pizzaItemGroup, String saucePrefabName, int itemReference, String label, String slicePath = "Slice", bool isSlice = false)
+        {
+            GameObject pizzaGo = pizzaItemGroup.Prefab;
+            ItemGroupView itemGroupView = pizzaGo.GetComponent<ItemGroupView>();
+            List<GameObject> alfredoSauces = new List<GameObject>();
+
+            if (isSlice)
+            {
+                GameObject sauceGo = GameObject.Instantiate(Bundle.LoadAsset<GameObject>(saucePrefabName)).AssignMaterialsByNames();
+                sauceGo.transform.SetParent(pizzaGo.transform.Find(slicePath), false);
+                itemGroupView.ComponentGroups.Add(new()
+                {
+                    DrawAll = true,
+                    GameObject = sauceGo,
+                    Item = (Item)GDOUtils.GetExistingGDO(itemReference),
+                });
+                itemGroupView.ComponentGroups.Add(new()
+                {
+                    DrawAll = true,
+                    GameObject = pizzaGo.transform.Find(slicePath + "/Sauce").gameObject,
+                    Item = (Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce),
+                });
+            }
+            else
+            {
+                foreach (ItemGroupView sliceItemGroupView in pizzaGo.GetComponentsInChildren<ItemGroupView>())
+                {
+                    GameObject sauceGo = GameObject.Instantiate(Bundle.LoadAsset<GameObject>(saucePrefabName)).AssignMaterialsByNames();
+                    sauceGo.transform.SetParent(sliceItemGroupView.transform.Find(slicePath), false);
+                    alfredoSauces.Add(sauceGo);
+                }
+
+                // Add roux + sauce game object to component groups, replicating tomato sauce
+                itemGroupView.ComponentGroups.Add(new()
+                {
+                    DrawAll = true,
+                    Objects = alfredoSauces,
+                    Item = (Item)GDOUtils.GetExistingGDO(itemReference),
+                });
+            }
+
+            AddColorblindLabel(pizzaItemGroup, itemReference, label);
+        }
+        
+        public void SetupPizzaItemSets(ItemGroup pizzaItemGroup, int itemReference)
+        {
+            ItemSet saucesItemSet = default(ItemSet);
+            int sauceItemSetIndex = -1;
+
+            ItemSet pizzaItemSet = default(ItemSet);
+            int index = -1;
+            for (int i = 0; i < pizzaItemGroup.DerivedSets.Count; i++)
+            {
+                ItemSet itemSet = pizzaItemGroup.DerivedSets[i];
+                if (itemSet.Items.Contains((Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce)) && itemSet.Items.Contains((Item)GDOUtils.GetExistingGDO(ItemReferences.PizzaCrust)))
+                {
+                    pizzaItemSet = itemSet;
+                    index = i;
+                }
+                else if (itemSet.Items.Contains((Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce)))
+                {
+                    saucesItemSet = itemSet;
+                    sauceItemSetIndex = i;
+                }
+            }
+            if (!pizzaItemSet.Equals(default(ItemSet)) && index != -1)
+            {
+                Item tomatoSauce = (Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce);
+                if (pizzaItemSet.Items.Contains(tomatoSauce))
+                {
+                    pizzaItemSet.Items.Remove(tomatoSauce);
+                    pizzaItemSet.Max = 1;
+                    pizzaItemSet.Min = 1;
+                    pizzaItemGroup.DerivedSets[index] = pizzaItemSet;
+                }
+                pizzaItemGroup.DerivedSets.Add(new()
+                {
+                    Max = 1,
+                    Min = 1,
+                    RequiresUnlock = false,
+                    IsMandatory = true,
+                    Items = new List<Item>()
+                    {
+                        (Item)GDOUtils.GetExistingGDO(itemReference),
+                        (Item)GDOUtils.GetExistingGDO(ItemReferences.TomatoSauce)
+                    }
+                });
+            }
+
+            if (!saucesItemSet.Equals(default(ItemSet)) && sauceItemSetIndex != -1)
+            {
+                saucesItemSet.Items.Add((Item)GDOUtils.GetExistingGDO(itemReference));
+                pizzaItemGroup.DerivedSets[sauceItemSetIndex] = saucesItemSet;
+            }
         }
     }
 }
